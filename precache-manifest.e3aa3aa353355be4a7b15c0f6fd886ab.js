8a2bf163b33c318b4d162f97105f86cd664517a9
pechaprasanna <rpv31dec1991@gmail.com>
2020-04-12 04:31:39 -0700

    Updates

---
+++ precache-manifest.e3aa3aa353355be4a7b15c0f6fd886ab.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e4cddb4742eb8f85e5afa85ffaa8cef",
+    "url": "/robotfriends/index.html"
+  },
+  {
+    "revision": "3a89509558486d66239d",
+    "url": "/robotfriends/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "058cc4816f5a0b914e7f",
+    "url": "/robotfriends/static/css/main.0a17ee8a.chunk.css"
+  },
+  {
+    "revision": "3a89509558486d66239d",
+    "url": "/robotfriends/static/js/2.2f7a48c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robotfriends/static/js/2.2f7a48c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "058cc4816f5a0b914e7f",
+    "url": "/robotfriends/static/js/main.ef71d6ce.chunk.js"
+  },
+  {
+    "revision": "b93df69794410b9252d3",
+    "url": "/robotfriends/static/js/runtime-main.e4d1301c.js"
+  },
+  {
+    "revision": "76a087ccd962305e48648165f86fa40d",
+    "url": "/robotfriends/static/media/Crazy Cartoon.76a087cc.woff"
+  },
+  {
+    "revision": "2c84a11cbaeabb9b2adebec946cbdb56",
+    "url": "/robotfriends/static/media/Spongeboy Me Bob.2c84a11c.woff"
+  }
+]);
